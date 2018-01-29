0117fa3ccbe5c150fdbd5e5371c0fbad8b5b6e3a
ez3ron <tanzerlaubnis@gmail.com>
2018-01-29 19:47:46 +0100

    button test py  1

---
+++ button.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+X=11
+Y=15
+
+GPIO.setup(X, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(Y, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_stateA = GPIO.input(X)
+    input_stateB = GPIO.input(Y)
+
+    if input_stateA == False:
+        print('Button Pressed')
+        time.sleep(0.2)
+
+    if input_stateB == False:
+        print('Button Pressed')
+        time.sleep(0.2)

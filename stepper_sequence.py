8e4c928afc234601b510fd75b2bc0e4e5200ebc0
ez3ron <tanzerlaubnis@gmail.com>
2018-01-30 01:51:42 +0100

    ver2 major update 1

--- stepper_sequence.py
+++ stepper_sequence.py
@@ -8,25 +8,27 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 # Verwendete Pins am Rapberry Pi
+# Motor 1:
 A=12   # in1
 B=16   # in2
 C=18   # in3
 D=22   # in4
 
-mo1 = 'A'
-mo2 = 'B'
-
+# Motor 2:
 E=29    #in1
 F=31    #in2
 G=33    #in3
 H=37    #in4
+
+#################################
+mo1 = 'A'
+mo2 = 'B'
 
 m1 = [A, B, C, D]
 m2 = [E, F, G, H]
 
 # zeit zwischen sequenzen
 time = 0.0005
-
 
 full = 512
 halF = 256
@@ -59,170 +61,99 @@ GPIO.output(G, False)
 GPIO.output(H, False)
 
 # Schritte 1 - 8 festlegen
-def Step1(m[]):
+def Step1(m = m[]):
     GPIO.output(m[3], True)
     sleep (time)
     GPIO.output(m[3], False)
 
-def Step2(m[]):
+def Step2(m = []):
     GPIO.output(m[2], True)
-    GPIO.output(D, True)
+    GPIO.output(m[3], True)
     sleep (time)
-    GPIO.output(D, False)
-    GPIO.output(C, False)
+    GPIO.output(m[3], False)
+    GPIO.output(m[2], False)
 
-def Step2B():
-    GPIO.output(G, True)
-    GPIO.output(H, True)
+def Step3(m = []):
+    GPIO.output(m[2], True)
     sleep(time)
-    GPIO.output(H, False)
-    GPIO.output(G, False)
+    GPIO.output(m[2], False)
 
-def Step3():
-    GPIO.output(C, True)
+def Step4(m = []):
+    GPIO.output(m[1], True)
+    GPIO.output(m[2], True)
     sleep(time)
-    GPIO.output(C, False)
+    GPIO.output(m[1], False)
+    GPIO.output(m[2], False)
 
-def Step3B():
-    GPIO.output(G, True)
+def Step5(m = []):
+    GPIO.output(m[1], True)
     sleep(time)
-    GPIO.output(G, False)
+    GPIO.output(m[1], False)
 
-def Step4():
-    GPIO.output(B, True)
-    GPIO.output(C, True)
+def Step6(m = []):
+    GPIO.output(m[0], True)
+    GPIO.output(m[1], True)
     sleep(time)
-    GPIO.output(B, False)
-    GPIO.output(C, False)
+    GPIO.output(m[0], False)
+    GPIO.output(m[1], False)
 
-def Step4B():
-    GPIO.output(F, True)
-    GPIO.output(G, True)
+def Step7(m = []):
+    GPIO.output(m[0], True)
     sleep(time)
-    GPIO.output(F, False)
-    GPIO.output(G, False)
+    GPIO.output(m[0], False)
 
-def Step5():
-    GPIO.output(B, True)
+def Step8(m = []):
+    GPIO.output(m[3], True)
+    GPIO.output(m[0], True)
     sleep(time)
-    GPIO.output(B, False)
+    GPIO.output(m[3], False)
+    GPIO.output(m[0], False)
 
-def Step5B():
-    GPIO.output(F, True)
-    sleep(time)
-    GPIO.output(F, False)
-
-def Step6():
-    GPIO.output(A, True)
-    GPIO.output(B, True)
-    sleep(time)
-    GPIO.output(A, False)
-    GPIO.output(B, False)
-
-def Step6B():
-    GPIO.output(E, True)
-    GPIO.output(F, True)
-    sleep(time)
-    GPIO.output(E, False)
-    GPIO.output(F, False)
-
-def Step7():
-    GPIO.output(A, True)
-    sleep(time)
-    GPIO.output(A, False)
-
-def Step7B():
-    GPIO.output(E, True)
-    sleep (time)
-    GPIO.output(E, False)
-
-def Step8():
-    GPIO.output(D, True)
-    GPIO.output(A, True)
-    sleep(time)
-    GPIO.output(D, False)
-    GPIO.output(A, False)
-
-def Step8B():
-    GPIO.output(H, True)
-    GPIO.output(E, True)
-    sleep(time)
-    GPIO.output(H, False)
-    GPIO.output(E, False)
-
+################################################
 # Volle Umdrehung
-def sequee1 ():
-        Step1()
-        Step2()
-        Step3()
-        Step4()
-        Step5()
-        Step6()
-        Step7()
-        Step8()
+def vorseq (m = []):
+        Step1(m)
+        Step2(m)
+        Step3(m)
+        Step4(m)
+        Step5(m)
+        Step6(m)
+        Step7(m)
+        Step8(m)
 
-def sequee2 ():
-        Step8()
-        Step7()
-        Step6()
-        Step5()
-        Step4()
-        Step3()
-        Step2()
-        Step1()
-
-def sequee1B ():
-        Step1B()
-        Step2B()
-        Step3B()
-        Step4B()
-        Step5B()
-        Step6B()
-        Step7B()
-        Step8B()
-
-def sequee2B ():
-        Step8B()
-        Step7B()
-        Step6B()
-        Step5B()
-        Step4B()
-        Step3B()
-        Step2B()
-        Step1B()
+def rukseq (m = []):
+        Step8(m)
+        Step7(m)
+        Step6(m)
+        Step5(m)
+        Step4(m)
+        Step3(m)
+        Step2(m)
+        Step1(m)
 
 ################################################
 # Vor / Rückdreuhung
-def volldrehung (art , motor):
-    print("vorwarts!")
-    for i in range (art):
-       	if  motor == 'A':
-		sequee1()
-    	else:
-		sequee1B()
-        print (i)
-
-def ruckdrehung (art, motor):
-    print("ruckwarts!")
-    for i in range (art):
-	if  motor == 'A':
-		sequee2()
-    	else:
-		sequee2B()
-        print (i)
+def action_turn (lenge , motor, direc):
+    for i in range (lenge):
+        if (direc == 'vor'):
+            print("VORWAERTS  <= !!")
+            vorseq(m)
+        elif (direc == 'ruk'):
+            print("RUCKWAERTS <= !!")
+            ruckseq(m)
+    print (i)
 
 ##############################################
-# stop & clear
+# stop & clear:
 def stop():
     print("STOP!")
     GPIO.cleanup()
 
 # Hauptprogramm:
 try:
-    volldrehung(full, m1)
-    ruckdrehung(full, m2)
-    volldrehung(vrtl, m1)
-    ruckdrehung(vrtl, m2)
+    action_turn(full, m1, 'vor')
+    action_turn(full, m2, 'ruk')
+    
 except KeyboardInterrupt:
     stop()
 finally:

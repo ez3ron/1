bf7c59c7e641954c281cc829944f884b42b65f66
ez3ron <tanzerlaubnis@gmail.com>
2018-01-30 13:29:39 +0100

    zähler ver 1 einführung

--- button.py
+++ button.py
@@ -6,6 +6,8 @@ GPIO.setmode(GPIO.BOARD)
 X=11
 Y=15
 
+INTER=[]
+
 GPIO.setup(X, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(Y, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
@@ -20,10 +22,12 @@ try:
         input_stateB = GPIO.input(Y)
 
         if input_stateA == False:
+            INTER[0] + 1
             print('Button Pressed AA')
             time.sleep(0.2)
 
         if input_stateB == False:
+            INTER[1] + 1
             print('Button Pressed BB')
             time.sleep(0.2)
 except KeyboardInterrupt:

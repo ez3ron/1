f82238ef5dd1f0ba7f21af43078e2a433b4b1a98
ez3ron <tanzerlaubnis@gmail.com>
2018-01-30 17:18:57 +0100

    zusam,enführung button

--- button.py
+++ button.py
@@ -6,8 +6,6 @@ GPIO.setmode(GPIO.BOARD)
 X=11
 Y=15
 
-INTER=[]
-
 GPIO.setup(X, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(Y, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
@@ -16,19 +14,18 @@ def stop():
     print("STOP!")
     GPIO.cleanup()
 
-try:
-    while True:
-        input_stateA = GPIO.input(X)
-        input_stateB = GPIO.input(Y)
+def buttons():
+    try:
+        while True:
+            input_stateA = GPIO.input(X)
+            input_stateB = GPIO.input(Y)
 
-        if input_stateA == False:
-            INTER[0] + 1
-            print('Button Pressed AA')
-            time.sleep(0.2)
+            if input_stateA == False:
+                print('Button Pressed AA')
+                time.sleep(0.2)
 
-        if input_stateB == False:
-            INTER[1] + 1
-            print('Button Pressed BB')
-            time.sleep(0.2)
-except KeyboardInterrupt:
-    stop()
+            if input_stateB == False:
+                print('Button Pressed BB')
+                time.sleep(0.2)
+    except KeyboardInterrupt:
+        stop()

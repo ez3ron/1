3522255f2bec29ae4fb4296065c44b887731e511
ez3ron <tanzerlaubnis@gmail.com>
2018-02-15 23:34:20 +0100

    d

--- dvddrive.py
+++ dvddrive.py
@@ -1,59 +1,3 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
 import RPi.GPIO as GPIO
 import time
  
@@ -63,7 +7,7 @@ coil_A_1_pin = 4 # pink
 coil_A_2_pin = 17 # orange
 coil_B_1_pin = 23 # blau
 coil_B_2_pin = 24 # gelb
-#enable_pin   = 7 # Nur bei bestimmten Motoren benoetigt (+Zeile 24 und 30)
+enable_pin   = 7 # Nur bei bestimmten Motoren benoetigt (+Zeile 24 und 30)
  
 # anpassen, falls andere Sequenz
 StepCount = 8
@@ -77,13 +21,13 @@ Seq[5] = [1,0,1,0]
 Seq[6] = [0,0,1,0]
 Seq[7] = [0,1,1,0]
  
-#GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
  
-#GPIO.output(enable_pin, 1)
+GPIO.output(enable_pin, 1)
 def setStep(w1, w2, w3, w4):
     GPIO.output(coil_A_1_pin, w1)
     GPIO.output(coil_A_2_pin, w2)

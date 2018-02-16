115059555deb24cff90769536e1a0865c8f12e12
ez3ron <tanzerlaubnis@gmail.com>
2018-02-16 01:38:57 +0100

    d

--- dvddrive.py
+++ dvddrive.py
@@ -10,11 +10,12 @@ coil_B_2_pin = 24 # gelb
 #enable_pin   = 7 # Nur bei bestimmten Motoren benoetigt (+Zeile 24 und 30)
  
 # anpassen, falls andere Sequenz
-StepCount = 2
+StepCount = 4
 Seq = list(range(0, StepCount))
-Seq[0] = [1,1,0,0]
-Seq[1] = [0,0,1,1]
-
+Seq[0] = [1,0,0,1]
+Seq[1] = [1,1,0,0]
+Seq[2] = [0,1,1,0]
+Seq[3] = [0,0,1,1]
 
 #GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
